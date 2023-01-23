7195fe6a336d5f35376373a5cb2fc471ffd7bdf2
figgge <fredrik.kvarnasen@gmail.com>
2023-01-23 11:38:16 +0100

    Add tests for Employee class

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +11,49 @@ class EmployeeTest {
     @BeforeEach
     void setUp() {
         employee = new Employee("1", 10000);
+    }
+
+    @Test
+    void testGetIdShouldReturnSetUpValue() {
+        assertEquals("1", employee.getId());
+    }
+
+    @Test
+    void testSetIdShouldReturnGivenValue() {
+        employee.setId("2");
+
+        assertEquals("2", employee.getId());
+    }
+
+    @Test
+    void testGetSalaryShouldReturnSetUpValue() {
+        assertEquals(10000, employee.getSalary());
+    }
+
+    @Test
+    void testSetSalaryShouldReturnGivenValue() {
+        employee.setSalary(20000);
+
+        assertEquals(20000, employee.getSalary());
+    }
+
+    @Test
+    void testIsPaidShouldReturnFalse() {
+        assertFalse(employee.isPaid());
+    }
+
+    @Test
+    void testSetPaidShouldReturnGivenValue() {
+        employee.setPaid(true);
+
+        assertTrue(employee.isPaid());
+    }
+
+    @Test
+    void testToString() {
+        var expected = "Employee [id=" + employee.getId() + ", salary=" + employee.getSalary() + "]";
+
+        assertEquals(expected, employee.toString());
     }
 
 
